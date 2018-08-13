77c9174a2a79becdb347164c39202bdcabe71411
highdevilxx@hotmail.com Khaled <highdevilxx@hotmail.com>
2018-08-13 07:54:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,10 +22,10 @@ function nowServing(line)
 function currentLine(line) {
    let i = 0;
    while (i < line.length) {
-     line.push(` `+[i+1]+`. `  + katzDeli[i])
+     line.push(` `+[i+1]+`. `  + line[i])
      i++;
    }
-   if (katzDeli.length === 0) {
+   if (line.length === 0) {
      return "The line is currently empty.";
    } else
    return(`The line is currently:` + line);
