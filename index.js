dbe96ab1264e96379e563953f46e2a94aa3f6f80
highdevilxx@hotmail.com Khaled <highdevilxx@hotmail.com>
2018-08-13 07:48:32 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,38 @@
+function takeANumber(line,name)
+{
+  var i = line.length+1;
+  line.push(name);
+  return "Welcome, "+ name+". You are number "+i+" in line.";
+}
+function nowServing(line)
+{
+  if(line.length===0)
+  {
+    return "There is nobody waiting to be served!";
+  }
+  else
+  {
+    line = line.shift();
+    var serv=line[0];
+    return "Currently serving "+line+ '.';
+
+  }
+
+}
+function currentLine(line)
+{
+  var i =0;
+  if(line.length===0)
+  {
+    return "The line is currently empty." ;
+  }
+  else
+  {
+    while(i<line.length)
+    {
+      line.push(` `+[i+1]+`. `  + line[i]);
+      i++;
+    }
+      return "The line is currently:"+line;
+  }
+}
