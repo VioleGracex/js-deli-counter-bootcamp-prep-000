6c197cad56e321259f17ae4f59331cb989e405de
highdevilxx@hotmail.com Khaled <highdevilxx@hotmail.com>
2018-08-13 07:54:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,6 +19,17 @@ function nowServing(line)
   }
 
 }
+function currentLine(katzDeli) {
+   let i = 0;
+   while (i < katzDeli.length) {
+     line.push(` `+[i+1]+`. `  + katzDeli[i])
+     i++;
+   }
+   if (katzDeli.length === 0) {
+     return "The line is currently empty.";
+   } else
+   return(`The line is currently:` + line);
+ }
 /*function currentLine(line)
 {
   var i =0;
